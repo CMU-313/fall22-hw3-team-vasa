a6fe6e603bdb93aa33897a53e2d8d6848dda3537
Stephanie Liu <sl3@andrew.cmu.edu>
2022-10-05 18:30:09 -0400

    added unit test for status field

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -3,10 +3,13 @@ package com.sismics.docs.core.dao.jpa;
 import com.sismics.docs.BaseTransactionalTest;
 import com.sismics.docs.core.dao.UserDao;
 import com.sismics.docs.core.model.jpa.User;
+import com.sismics.docs.core.dao.DocumentDao;
+import com.sismics.docs.core.model.jpa.Document;
 import com.sismics.docs.core.util.TransactionUtil;
 import com.sismics.docs.core.util.authentication.InternalAuthenticationHandler;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Date;
 
 /**
  * Tests the persistance layer.
@@ -35,5 +38,25 @@ public class TestJpa extends BaseTransactionalTest {
 
         // Authenticate using the database
         Assert.assertNotNull(new InternalAuthenticationHandler().authenticate("username", "12345678"));
+
+
+        // Create a new document
+        DocumentDao docDao = new DocumentDao();
+        Document doc = new Document();
+        Assert.assertEquals(0, doc.getStatus());
+        doc.setTitle("My new document");
+        doc.setDescription("My description");
+        doc.setCreateDate(new Date());
+        doc.setLanguage("eng");
+        doc.setUserId("me");
+        doc.setStatus(2);
+        String docId = docDao.create(doc, "me");
+
+        TransactionUtil.commit();
+
+        // Search a document by its ID and make sure the status is correct
+        doc = docDao.getById(docId);
+        Assert.assertNotNull(doc);
+        Assert.assertEquals(2, doc.getStatus());
     }
 }
